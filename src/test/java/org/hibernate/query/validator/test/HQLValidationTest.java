f217233ab5c431631990debb58874ba6ade9e759
gavinking <gavin@ceylon-lang.org>
2019-03-22 01:05:10 +0100

    remove uncompilable code

--- src/test/java/org/hibernate/query/validator/test/HQLValidationTest.java
+++ src/test/java/org/hibernate/query/validator/test/HQLValidationTest.java
@@ -11,16 +11,16 @@ public class HQLValidationTest {
         createQuery("from Person p join p.address a where a.city='barcelona'");
         createQuery("from Person p where p.address.city='barcelona'");
 
-        createQuery("do");
-        createQuery("from");
-        createQuery("from Person where p.name='gavin' select");
-        createQuery("from Person p where p.name+='gavin'");
-        createQuery("select from Person where p.name='gavin'");
-
-        createQuery("from People p where p.name='gavin'");
-        createQuery("from Person p where p.firstName='gavin'");
-        createQuery("from Person p join p.addr a");
-        createQuery("from Person p where p.address.town='barcelona'");
+//        createQuery("do");
+//        createQuery("from");
+//        createQuery("from Person where p.name='gavin' select");
+//        createQuery("from Person p where p.name+='gavin'");
+//        createQuery("select from Person where p.name='gavin'");
+//
+//        createQuery("from People p where p.name='gavin'");
+//        createQuery("from Person p where p.firstName='gavin'");
+//        createQuery("from Person p join p.addr a");
+//        createQuery("from Person p where p.address.town='barcelona'");
     }
 
     private static void createQuery(String s) {}
